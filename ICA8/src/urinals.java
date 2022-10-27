31000b891d6a73ecf22e229617043487ddce4316
Subham Kumar <skuma251@asu.edu>
2022-10-27 11:12:17 -0700

    Created urinals class with only main function

--- ICA8/src/urinals.java
+++ ICA8/src/urinals.java
@@ -1,5 +1,11 @@
-public class Main {
+/**
+ * @author Subham Kumar
+ */
+
+public class urinals {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
     }
 }
