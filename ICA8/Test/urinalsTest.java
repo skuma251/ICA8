41d0866b2be4c70093611587eefa1ebf426c0593
Subham Kumar <skuma251@asu.edu>
2022-10-27 15:35:03 -0700

    Created a test case which fails

---
+++ ICA8/Test/urinalsTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @Test
+    void goodString() {
+        urinals testObj = new urinals();
+        System.out.println("====== Subham Kumar == TEST ONE EXECUTED ======");
+        assertEquals(true, testObj.goodString("0100"));
+    }
+}
--- ICA8/src/urinals.java
+++ ICA8/src/urinals.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 /**
  * @author Subham Kumar
  */
 
 public class urinals {
     public static void main(String[] args) {
+        urinals newUrinal = new urinals();
+        String inputUrinals = newUrinal.getInputKeyboard();
+    }
 
-        System.out.println("Hello world!");
-
+    public Boolean goodString( String str ) {
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) != '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+    public String getInputKeyboard() {
+        System.out.println("Enter 1 for occupied and 0 for unoccupied urinals");
+        System.out.println("Enter the input representing the urinals.");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        return input;
     }
 }
