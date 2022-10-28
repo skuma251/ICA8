9e4621ea292b05e98241cd99f758d293165ecbe9
Subham Kumar <skuma251@asu.edu>
2022-10-27 18:19:32 -0700

    Created a test case for counting number of free urinals which fails

--- ICA8/Test/urinalsTest.java
+++ ICA8/Test/urinalsTest.java
@@ -9,6 +9,13 @@ class urinalsTest {
         urinals testObj = new urinals();
         System.out.println("====== Subham Kumar == TEST ONE EXECUTED ======");
         assertEquals(true, testObj.goodString("0101"));
-        assertEquals(true, testObj.goodString("0110"));
+        assertEquals(false, testObj.goodString("0110"));
+    }
+
+    @Test
+    void countFreeUrinals() {
+        urinals testObj = new urinals();
+        System.out.println("====== Subham Kumar == TEST TWO EXECUTED ======");
+        assertEquals(1, testObj.countFreeUrinals("0100"));
     }
 }
--- ICA8/src/urinals.java
+++ ICA8/src/urinals.java
@@ -8,22 +8,47 @@ public class urinals {
     public static void main(String[] args) {
         urinals newUrinal = new urinals();
         String inputUrinals = newUrinal.getInputKeyboard();
-        System.out.println(newUrinal.goodString(inputUrinals));
+        int count = newUrinal.countFreeUrinals(inputUrinals);
+        System.out.println(count);
     }
 
-    public Boolean goodString( String str ) {
+    public Boolean goodString(String str) {
         boolean flag = true;
-        for(int i=1; i<str.length()-1; i++) {
-            if (str.charAt(i) == '1' && str.charAt(i-1) == '1' || str.charAt(i) == '1' && str.charAt(i+1) == '1')
-                flag =  false;
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '1' && str.charAt(i - 1) == '1' || str.charAt(i) == '1' && str.charAt(i + 1) == '1')
+                flag = false;
         }
         return flag;
     }
+
     public String getInputKeyboard() {
         System.out.println("Enter 1 for occupied and 0 for unoccupied urinals");
         System.out.println("Enter the input representing the urinals.");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         return input;
+    }
+
+    public int countFreeUrinals(String inputUrinals) {
+        int count = 0;
+        char ch = '1';
+        String str = inputUrinals;
+        for (int i = 0; i < str.length(); i++) {
+            Boolean flag = false;
+            if (i == 0 && str.charAt(i) == '0') {
+                if(str.charAt(i+1) == '0'){
+                    count++;
+                }
+            } else if (i == str.length() - 1 && str.charAt(i) == '0' && str.charAt(i - 1) == '0' ) {
+                count++;
+            } else if (str.charAt(i) == '0' && str.charAt(i - 1) == '0' && str.charAt(i + 1) == '0') {
+                count++;
+            }
+            StringBuilder string = new StringBuilder(str);
+            string.setCharAt(i, ch);
+            str = String.valueOf(string);
+
+        }
+        return count;
     }
 }
